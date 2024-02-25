7afaa9bd1ab24a0fa4e7b9b10069a9738cf57905
Mehriban Shukrieva <127889525+marynotfound@users.noreply.github.com>
2024-02-25 14:16:47 +0300

    Insertion Sort.c

---
+++ Insertion Sort.c
@@ -0,0 +1,29 @@
+#include <math.h>
+#include <stdio.h>
+ 
+//Insertion sort
+void insertionSort(int arr[], int n)
+{
+    int i, key, j;
+    for (i = 1; i < n; i++) {
+        key = arr[i];
+        j = i - 1;
+ 
+        // Move elements of the arr,which are greater than key, to one position ahead of their current position 
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
+}
+ 
+// A function to Print the array 
+void printArray(int arr[], int n)
+{
+    int i;
+    for (i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+ 
